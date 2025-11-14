5a9a79d42e74d88a16417eb9f94cec1774775d3f
krishurmi12 <krishan.krishan.rajput460@gmail.com>
2025-11-15 00:47:37 +0530

    commit message

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-hii
+>> for 1st time configure github in our system
+
+git config --global user.name "add git username"
+git config --global user.email "add email id that you create when you create git hub"
+
+git init "this is for initialise "
+
+git remote add origin add url of repository "this is final setting now no need to more command prompt"
